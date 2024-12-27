ee6934c888ddb76d30bed16d9d8c528f74343374
Lucas Silva <lucassilvax005x@gmail.com>
2024-12-27 00:20:55 -0300

    feat: adding integration and unit tests for user creation and basic arithmetic

---
+++ src/__tests__/test.unit.test.ts
@@ -0,0 +1,7 @@
+describe('when we try to add two numbers', () => {
+  it('should return the sum of the two numbers', () => {
+    const a = 1;
+    const result = a + a;
+    expect(result).toBe(2);
+  });
+});
