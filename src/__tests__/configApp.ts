38dde131edbda68356f5ba109d7330197a4e1c6a
Lucas Silva <lucassilvax005x@gmail.com>
2025-02-07 19:30:23 -0300

    feat: adding up integration test utilities and application configuration

---
+++ src/__tests__/configApp.ts
@@ -0,0 +1,23 @@
+import path from "path";
+
+import { Server } from "../domain/server/server";
+import { BoilerplateController } from "../application/controllers/boilerplate.controller";
+import { UserController } from "../application/controllers/user.controller";
+import { BoilerplateService } from "../domain/boilerplate/boilerplate.service";
+import { UserService } from "../domain/user/user.service";
+import { UserRepository } from "../infraestructure/repository/user.repository";
+
+const OPEN_API_SPEC_FILE_LOCATION = path.resolve(
+  __dirname,
+  "../contracts/service.yaml"
+);
+
+export const app = new Server({
+  port: Number(process.env.PORT) || 3000,
+  controllers: [
+    new BoilerplateController(new BoilerplateService()),
+    new UserController(new UserService(new UserRepository())),
+  ],
+  databaseURI: process.env.DATABASE_URI,
+  apiSpecLocation: OPEN_API_SPEC_FILE_LOCATION,
+});
